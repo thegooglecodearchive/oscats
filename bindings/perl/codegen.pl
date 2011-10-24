98f8fe79329732d0fd0d17f2f2a94b93f6c6ab80
Michael Culbertson <Michael Culbertson@localhost>
2011-10-23 22:44:03 -0500

    Add GPerlBoxedWrapperClass for GValueArray Workaround for https://bugzilla.gnome.org/show_bug.cgi?id=662265

--- bindings/perl/codegen.pl
+++ bindings/perl/codegen.pl
@@ -141,11 +141,118 @@ print OUT <<"END";
 #include <oscats.h>
 #include "oscats-autogen.h"
 
+SV * gperl_value_array_wrap(GType gtype, const char * package,
+                            gpointer boxed, gboolean own)
+{
+  GValueArray *array = (GValueArray*)boxed;
+  AV *av;
+  SV *sv;
+  guint i;
+  
+  av = newAV();
+  for (i=0; i < array->n_values; i++)
+  {
+    GValue *value = array->values+i;
+    sv = gperl_sv_from_value(value);
+    av_push(av, sv);
+    if (own)
+    {
+      if (G_VALUE_HOLDS_BOXED(value))
+        g_boxed_free(G_VALUE_TYPE(value), g_value_get_boxed(value));
+      else if (G_VALUE_HOLDS_OBJECT(value))
+        g_object_unref(g_value_get_object(value));
+    }
+  }
+  sv = newRV_noinc((SV*)av);
+
+  return sv;
+}
+
+gpointer gperl_value_array_unwrap(GType gtype, const char *package, SV *sv)
+{
+  GValueArray *array;
+  GValue value = {0, };
+  AV *av;
+  int i, num;
+  
+  g_return_val_if_fail(SvROK(sv) && SvTYPE(SvRV(sv)) == SVt_PVAV, NULL);
+  av = (AV*) SvRV(sv);
+  num = av_len(av)+1;
+  
+  array = g_value_array_new(num);
+  for (i=0; i < num; i++)
+  {
+    SV **ee = av_fetch(av, i, FALSE);
+    SV *e = (ee ? *ee : NULL);
+    g_return_val_if_fail(e != NULL, NULL);
+
+    if (SvROK(e))
+    {
+        g_value_init(&value, gperl_type_from_package(sv_reftype(SvRV(e), TRUE)));
+        if (G_VALUE_HOLDS_OBJECT(&value))
+          g_value_set_object(&value,
+            gperl_get_object_check(e, G_VALUE_TYPE(&value)));
+        else if (G_VALUE_HOLDS_BOXED(&value))
+          g_value_set_boxed(&value,
+            gperl_get_boxed_check(e, G_VALUE_TYPE(&value)));
+        else
+          g_warning("gperl_value_array_unwrap: What do I do here?");
+    }
+    else switch (SvTYPE(e))
+    {
+      case SVt_IV:	// int
+        if (SvUOK(e))
+        {
+          g_value_init(&value, G_TYPE_INT);
+          g_value_set_int(&value, SvIV(e));
+        } else {
+          g_value_init(&value, G_TYPE_UINT);
+          g_value_set_uint(&value, SvUV(e));
+        }
+        break;
+        
+      case SVt_NV:	// double
+        g_value_init(&value, G_TYPE_DOUBLE);
+        g_value_set_double(&value, SvNV(e));
+        break;
+        
+      case SVt_PV:	// string
+        g_value_init(&value, G_TYPE_STRING);
+        g_value_set_string(&value, SvPV_nolen(e));
+        break;
+        
+      case SVt_PVMG:	// blessed/magical scalar
+      case SVt_PVAV:	// array
+      case SVt_PVHV:	// hash
+      case SVt_PVCV:	// code
+      case SVt_PVGV:	// glob
+      default:
+        g_warning("gperl_value_array_unwrap: Unsupported type for element %d", i);
+    }
+    
+    g_value_array_append(array, &value);
+    g_value_unset(&value);
+  }
+
+  return array;
+}
+
+void gperl_value_array_destroy (SV *sv)
+{
+  g_warning("gperl_value_array_destroy()");
+}
+
+GPerlBoxedWrapperClass valueArrayWrapper = {
+  gperl_value_array_wrap,
+  gperl_value_array_unwrap,
+  gperl_value_array_destroy };
+
 MODULE = $modname PACKAGE = $modname
 
 BOOT:
 #include "register-oscats.xsh"
   gperl_register_sink_func(OSCATS_TYPE_ALGORITHM, g_object_ref_sink);
+  gperl_register_boxed(G_TYPE_VALUE_ARRAY, "ARRAY", &valueArrayWrapper);
 
 END
 
